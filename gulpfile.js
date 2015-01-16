f6d98c1f30121450bde5cf2e6d1ee7b6c51ac886
Nikolay Sadovnikov <gruzzilkin@gmail.com>
2015-01-17 00:33:27 +0500

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
+var del = require('del');
 var buildBranch = require('buildbranch');
 
-gulp.task('cname', function () {
-    return gulp.src('CNAME').pipe(gulp.dest('dist'));
-});
-
 gulp.task('clean', function (cb) {
-    del(['./dist'], cb);
+    del(['./www'], cb);
 });
 
-gulp.task('build', ['cname']);
+gulp.task('copy', function (cb) {
+    return gulp.src('src/**/*').pipe(gulp.dest('www'));
+});
+
+gulp.task('build', ['copy']);
 
 gulp.task('gh', function(done) {
     buildBranch({ ignore: ['libs', 'src'] }, done);
