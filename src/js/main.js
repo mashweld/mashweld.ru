cd87f73d80e99b2fce1c17a65c6e2ce869c7a9a1
Nikolay Sadovnikov <gruzzilkin@gmail.com>
2015-02-04 00:46:31 +0500

    Change bg color and some minor improvements

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,28 @@
 
 	var customSelect = $('.form select');
 
+	if (!window.atob) {
+		window.atob = function(str) {
+			return Base64.decode(str);
+		}
+	}
+
 	if (customSelect.length > 0) {
 		customSelect.select2({
 			width: 'resolve'
 		});
 	}
+
+	/*$.ajax({
+		dataType: "json",
+		url: 'https://api.github.com/repos/GManzato/jsdebutant/contents/jquery/exemple.json',
+		success: function(data) {
+			var obj = jQuery.parseJSON( atob(data.content) );
+			for(var key in obj) {
+				$('#holder').append(obj[key]['first_name'] + ' ' + obj[key]['last_name']+'<br/>')
+			}
+		}
+	});*/
 
 
 }(window.jQuery);
