406e79f4e20a29b6dda6f1287518823c215ebe3f
gruzzilkin <gruzzilkin@gmail.com>
2026-03-12 00:18:55 +0300

    Add ESLint, Stylelint, and pug-lint with npm scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	{
+		files: ['gulpfile.js'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: 'commonjs',
+			globals: { ...globals.node },
+		},
+		rules: {
+			'no-var': 'off',
+		},
+	},
+	{
+		files: ['src/js/main.js'],
+		languageOptions: {
+			ecmaVersion: 5,
+			sourceType: 'script',
+			globals: {
+				...globals.browser,
+				jQuery: 'readonly',
+				Base64: 'readonly',
+			},
+		},
+		rules: {
+			'no-var': 'off',
+		},
+	},
+];
--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ function templates() {
 			}
 		}))
 		.pipe(htmlFilter)
-		.pipe(replace(/((src|href)=)(\"|\')((img|css|js))/g, '$1$3../$4'))
+		.pipe(replace(/((src|href)=)("|')((img|css|js))/g, '$1$3../$4'))
 		.pipe(htmlFilter.restore)
 		.pipe(gulp.dest('www'));
 }
